a536fb49ce3dba667e8755f0f0ef50b62ec8482d
shresthgupta9 <95098436+shresthgupta9@users.noreply.github.com>
2023-12-29 22:29:24 +0530

    added sign and updated required attribute for radio in form, also added media query

--- components/script.js
+++ components/script.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+// object constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,6 +8,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
+// reading from myLibrary array and displaying in .items container
 function displayBook() {
     const items = document.querySelector(".items");
     items.innerHTML = "";
@@ -40,12 +42,14 @@ function displayBook() {
     }
 };
 
+// adding book to myLibrary
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
     displayBook();
 };
 
+// deleting book from myLibrary
 function deleteBookFromLibrary(event) {
     if (event.target.classList.value === "delete") {
         let index = event.target.value;
@@ -54,18 +58,21 @@ function deleteBookFromLibrary(event) {
     }
 };
 
+// form pop-up show
 const add = document.querySelector("#addBtn");
 add.addEventListener("click", () => {
     const popUp = document.querySelector(".popup");
     popUp.style.display = "flex";
 });
 
+// form pop-up close
 const closeBtn = document.querySelector("#closeBtn");
 closeBtn.addEventListener("click", () => {
     const popUp = document.querySelector(".popup");
     popUp.style.display = "none";
 });
 
+// add book event listener
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -76,5 +83,6 @@ form.addEventListener("submit", (e) => {
     addBookToLibrary(title, author, pages, read);
 });
 
+// delete book event listener
 const itemsContainer = document.querySelector(".items");
 itemsContainer.addEventListener("click", deleteBookFromLibrary);
